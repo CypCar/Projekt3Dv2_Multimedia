a248c9bc5006e8cd142d7eda8d4e94244cc4fe83
CypCar <antaras100@gmail.com>
2023-06-09 03:40:46 +0200

    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    Slider forceSlider;
+    float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        forceSlider = GameObject.Find("ForceSlider").GetComponent<Slider>();
+        force = forceSlider.value;
+        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.AddForce(transform.forward*force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Target"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    GameObject[] allTargets;
+    public GameObject winPanel;
+    public TMP_Text timerText;
+    public TMP_Text timeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.timeSinceLevelLoad;
+        timerText.SetText(time.ToString("0.0"));
+
+        allTargets = GameObject.FindGameObjectsWithTag("Target");
+        Debug.Log(allTargets.Length);
+        if(allTargets.Length < 1)
+        {
+            Time.timeScale = 0;
+            winPanel.SetActive(true);
+            timerText.enabled = false;
+            timeText.SetText(time.ToString("0.0"));
+        }
+    }
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
